b973bf0da00294ec85ce18758e83ff073a4f8690
toderodavi <estrela.alem@gmail.com>
2025-11-01 01:15:07 -0300

    Cifra de vigenere

---
+++ crypt.py
@@ -0,0 +1,29 @@
+alfabeto = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!'(),-.:;?[]{}ÀÁÂÃàáâãÉÊéêÕÔõôÍíÚúÇç" + '"'
+alfabeto = alfabeto
+def mensagem_vigenere(mensagem): 
+    mensagem = mensagem
+    return mensagem
+
+def chave_vigenere(chave, tamanho):
+    chave = chave
+    texto_chave = (chave * (tamanho // len(chave) + 1 )) [:tamanho]
+    return texto_chave
+
+def cifra_vigenere(mensagem, chave):
+    # Texto em caixa alta
+    texto = mensagem_vigenere(mensagem)
+    # Chave da cifra
+    chave = chave_vigenere(chave, len(texto))
+    resultado = ''
+
+    for i in range(len(texto)):
+        j = texto[i]
+        k = chave[i]
+        if j in alfabeto and k in alfabeto:
+            posTexto = alfabeto.index(j)
+            posChave = alfabeto.index(k)
+            print(j, k, alfabeto[posTexto + posChave])
+            resultado += alfabeto[(posTexto + posChave) % len(alfabeto)]
+        else:
+            resultado += j
+    return resultado
